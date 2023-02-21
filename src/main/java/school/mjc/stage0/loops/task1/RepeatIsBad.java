275f6cdd236a2339fc203aefcc76c8a9e64281e5
Bakhovuddin Latifjanov <salokhiddin2016@Bakhovuddins-MacBook-Pro.local>
2023-02-21 23:40:04 +0500

    Repeat is bad

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -4,6 +4,28 @@ public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
+        String s1 = "writing the same code doesn't have much impact, and it's also time consuming";
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+        System.out.println(s1);
+
 
     }
 }
